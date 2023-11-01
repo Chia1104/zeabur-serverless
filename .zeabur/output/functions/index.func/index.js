68149bb5ef333e6cb5eddebfa02a5bf56ebbbe24
Chia1104 <yuyuchia7423@gmail.com>
2023-11-01 19:33:29 +0800

    fix: remove query params

--- .zeabur/output/functions/index.func/index.js
+++ .zeabur/output/functions/index.func/index.js
@@ -1,7 +1,5 @@
 module.exports = (req, res) => {
-  const { name = "World" } = req.query;
-
-  const text = `Hello ${name}!`;
+  const text = `Hello World!`;
   const body = JSON.stringify({ text });
 
   res.setHeader("Content-Type", "text/plain");
